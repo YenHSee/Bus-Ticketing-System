14b82dfade2047d7594f981e35014b3ccf17b04b
YenHSee <seeyhong@hotmail.com>
2017-01-08 22:01:29 +0800

    Update ForgetPassword.java
    1.Developers:[See Yen Hong]
    2.Time:[08/01/2017 9:53 PM]
    3.Use_case [#136618137], [Password Recovery Module]

--- ForgetPassword.java
+++ ForgetPassword.java
@@ -32,7 +32,7 @@ public class ForgetPassword extends javax.swing.JFrame {
         jtfAnswer.setEnabled(false);
         jbtSubmit.setEnabled(false);
     }
-
+//
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
